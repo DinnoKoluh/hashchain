f965fcf6ac202795b4c1b1ec56670b4afe18d597
DinnoKoluh <koluh.dinno@gmail.com>
2023-04-05 18:24:50 +0200

    Added toolbar and some styling. Added create account page and repo page.

--- bc_site/chain/urls.py
+++ bc_site/chain/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.base, name='base'),
     path('blockchain', views.blockchain, name='blockchain'),
+    path('create_account', views.create_account, name='create_account'),
 ]
--- bc_site/chain/views.py
+++ bc_site/chain/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
 def base(request):
+     """
+     Where the homepage/base site will be located.
+     """
      return render(request, 'base.html')
 
 def blockchain(request):
+     """
+     Where the blockchain visualization site will be located.
+     """
      return render(request, 'blockchain.html')
+
+def create_account(request):
+     """
+     Where the account creation site will be located.
+     """
+     if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log in the user
+            login(request, user)
+            # Redirect to a success page
+            return redirect('success')
+     else:
+          form = UserCreationForm()
+     return render(request, 'create_account.html', {'form': form})
